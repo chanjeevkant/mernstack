eec5af0c512da90e770e4871f43d471e1723bf03
Chanjeev kant k <78922183+chanjeevkant@users.noreply.github.com>
2021-07-10 11:23:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+// <p>A simple React app.....</p>
+//
+//         <a href="https://reactjs.org" target="_blank">
+//
+//
+//
+//           Learn React
+//         </a>
+//         <form action="/" method="post"
+//               className="form">
+//           <button type="submit">Connected?</button>
+//         </form>
+    <h3>Number of reviews </h3>
+    <p> {} </p>
+    </div>
+  );
+}
+
+
+
+export default App;
